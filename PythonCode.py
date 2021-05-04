af1655876820da00f9c8d879e699142cd43fcd17
Gradics <gradics@gmail.com>
2021-05-04 13:42:42 +0200

    Initial commit

---
+++ PythonCode.py
@@ -0,0 +1,7 @@
+import bt
+
+# Download historical prices
+bt_data = bt.get('fb, amzn, goog, nflx, aapl',
+               start='2020-12-1', end='2020-12-31')
+# Print the top five rows
+print(bt_data.head())
