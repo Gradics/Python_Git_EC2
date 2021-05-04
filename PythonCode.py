719c5beef1154c8a54a056ab75289571c371b51e
Gradics <gradics@gmail.com>
2021-05-04 13:50:14 +0200

    code changed

--- PythonCode.py
+++ PythonCode.py
@@ -4,4 +4,4 @@ import bt
 bt_data = bt.get('fb, amzn, goog, nflx, aapl',
                start='2020-12-1', end='2020-12-31')
 # Print the top five rows
-print(bt_data.head())
+print(bt_data.head(10))
